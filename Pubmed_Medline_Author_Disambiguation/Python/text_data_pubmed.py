3bd9df78530dc7c3b9e7a75ec1e6d3a684780fae
RTeresiOB <robert.teresi@yale.edu>
2020-08-03 17:06:50 -0700

    Large restructuring/refactoring of code
    Changed how MeSH data is cleaned.

--- Pubmed_Medline_Author_Disambiguation/Python/text_data_pubmed.py
+++ Pubmed_Medline_Author_Disambiguation/Python/text_data_pubmed.py
@@ -1,117 +1,179 @@
+# Import libraries
 import numpy as np
-#import psutil
 from datetime import datetime
 import os
+import shutil # Copy files to another directory
 import zipfile
 import xml.sax
 import multiprocessing
 import sys
 import pandas as pd
-import pubmed_parser
+from pubmed_parser import medline_parser
 import gzip
 import re
 from pathlib import Path
+from multiprocessing import Pool
+
+# Assign Global Variables
+# Set data directories
+# Text data dir is where the script finds the source xml file from the medline website
+text_data_dir = "/Volumes/LaCie/PubmedData_Update_2019/ftp.ncbi.nlm.nih.gov/pubmed/baseline/"
+# Res dir is where the compressed, clean csv equivalents of each xml file is stored 
+res_dir = "/Volumes/LaCie/PubmedData_Update_2019/Clean_Csvs/"
+# Combined dir is where 
+combined_dir = "/Volumes/LaCie/PubmedData_Update_2019/Full_Pubmed/"
+
+# THESE PATHS MUST BE ASSIGNED
+title_stop_path = "/Users/RobertTeresi/Documents/pubmed_small_stopwords_list.txt"
+affil_stop_path = "/Users/RobertTeresi/Documents/pubmed_affiliation_stopwords.txt"
+mesh_stop_path =  "/Users/RobertTeresi/Documents/pubmed_mesh_stopwords.txt"
 
 
-## Downloads zipped xml from the medline website
 def download_pubmed_data():
+    """Download zipped xml from the medline website."""
     from subprocess import call
     call("./pubmed_update")
     
-## Reads stoplist
-def read_stoplist(stoplistpath):  # a path to a .txt file with stopwords separated by ", "
-    with open(stoplistpath, 'r') as file:
-        data = file.read().split(', ')
-        return(data)
 
-## Extract an author's email address from their list of affiliations
-def extract_email_from_affiliations(affiliation):
-    try:
-        return(re.findall("[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+", affiliation)[0])
-    except:
-        return('')
+def process_data(filepath, filename, respath, rm_stopwords = True,
+                 stop_paths = None):
+    """Take zipped xml from medline and transform it into a clean, gzipped csv.
+    
+    rm_stopwords set true to remove appropriate stopwords from title,
+    affiliation, and mesh fields.
+    stop_paths = List or tuple of paths to title, affiliation, and mesh
+                stopwords in that order. Default is none - take paths from
+                main function, where the paths are defined globally
+    """
+    def clean_string(string, stopword_list = None,
+                 rm_stopwords = True, mesh =  False):
+        """Convert the string to lower and take out stopwords."""
+    
+        # If we don't supply a stopword list, but don't have remove stopwords set to False, we raise an error
+        if stopword_list is None and rm_stopwords:
+            raise Exception('Please Supply a Stopword List or set rm_stopwords to False')
+            
+        # Mesh listings need a slightly different treatment
+        if mesh: 
+            # Remove MeSH Codes
+            string = [ re.sub(r'([ ]?d\d{6}\:','',x) for x in string]
+            if rm_stopwords:
+                #stopword_list = [stops + '/' for stops in stopword_list] # This makes it easier to  get rid of extra semi-colons
+                # Remove stop words
+                string = [x for x in string if x not in stopword_list]
 
-## This function will convert the string to lower and take out stopwords
-def clean_string(string, stopword_list = None, rm_stopwords = True, mesh =  False):
-    
-    ## If we don't supply a stopword list, but don't have remove stopwords set to False, we raise an error
-    if stopword_list is None and rm_stopwords:
-        raise Exception('Please Supply a Stopword List or set rm_stopwords to False')
-        
-    ## First, convert the string to lower
-    string = string.lower()
-    
-    if mesh: ## The mesh listings need a slightly different treatment
-        if rm_stopwords:
+            # Convert to '/' separated string
+            string = '/'.join(string)
             
-            stopword_list = [stops + ';' for stops in stopword_list] # This makes it easier to  get rid of extra semi-colons
-            ## Remove stopwords
-            string = re.sub(r'([ ]?d\d{6}\:' + r'|[ ]?d\d{6}\:'.join(stopword_list) + r')', '', string)
-        
-        
-        ## Now I only want to keep the unique ID
-        string = re.sub(r'[ ]?(d\d{6})(\:[a-z\-\'\.\, ]+)', r'\1', string)
-        return(string)
-    else:       
-        string = re.sub(r'\W+|\b[a-z]\b', ' ', string)
-            
-            ## Now I only want to keep
-        ## Remove stopwords if the option is set to true
-        if rm_stopwords:  
+
+            #string = re.sub(r'([ ]?d\d{6}\:' + r'|[ ]?d\d{6}\:'.join(stopword_list) + r')', '', string)
+            # Now I only want to keep the unique ID
+            #string = re.sub(r'[ ]?(d\d{6})(\:[a-z\-\'\.\, ]+)', r'\1', string)
+            return(string)
+        else:       
+            # First, convert the string to lower
+            string = string.lower()
+            string = re.sub(r'\W+|\b[a-z]\b', ' ', string)
                 
-            string = re.sub(r'\b(' + r'| '.join(stopword_list) + r')\b\s*', ' ', string)
-            
-        ## Trim whitespaces (induced by cleaning or otherwise)
-        string = string.strip()                 # From beginning and end
-        string = re.sub(r'[ ]+'," ",string)     # From the middle
-        return(string)
+            # Remove stopwords if the option is set to true
+            if rm_stopwords:  
+                    
+                string = re.sub(r'\b(' + r'| '.join(stopword_list) + r')\b\s*', ' ', string)
+                
+            # Trim whitespaces (induced by cleaning or otherwise)
+            string = string.strip()                 # From beginning and end
+            string = re.sub(r'[ ]+'," ",string)     # From the middle
+            return(string)
+        
+    def extract_email_from_affiliations(affiliation):
+        """Extract an author's email address from their list of affiliations."""
+        try:
+            return(re.findall(r"[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+",
+                              affiliation)[0])
+        except:
+            return('')
+        
+    def read_stoplist(stoplistpath, sep = ", "):
+        """Parse our list of stopwords.
+        
+        stoplistpath = a path to a .txt file with stopwords
+        sep = Separator of words in list (separated by ", " by default)
+        """
+        with open(stoplistpath, 'r') as file:
+            data = file.read().split(sep)
+        return(data)
     
-    
-## Worker Function - takes compressed xml from medline and transforms it into a corresponding clean, compressed csv
-def process_data(filepath, filename, respath, rm_stopwords = True): #  rm_stopwords set true to remove appropriate stopwords from title, affiliatio, and mesh fields
     print(filename + " - start")
     begin = datetime.now()
     print(filename + " - load data")
-    df = pubmed_parser.parse_medline_xml(filepath)
+    df = medline_parser.parse_medline_xml(filepath)
     df = pd.DataFrame(df)
     print(filename + " - save to " + respath)
     
-    ## Get rid of some unneeded columns -- can maybe work this into the parser later
-    df = df.drop(['publication_types', 'chemical_list','keywords', 'doi', 'references',\
-             'delete','pmc','other_id','medline_ta','nlm_unique_id','issn_linking','country'],axis=1)
+    # Get rid of unneeded columns -- can maybe work this into the parser later
+    df = df.drop(['publication_types', 'chemical_list',
+                  'keywords', 'doi', 'references',
+                  'delete','pmc','other_id','medline_ta',
+                  'nlm_unique_id','issn_linking','country'],
+                 axis=1)
     
-    ## Replace missing values (mesh, affiliation, or title) with ''
-    df[['mesh_terms','affiliations','title']] = df[['mesh_terms','affiliations','title']].fillna('')
+    # Replace missing values (mesh, affiliation, or title) with ''
+    nacols = ['mesh_terms','affiliations','title']
+    df[nacols] = df[nacols].fillna('')
     
-    ## Apparently only some of the xmls read in the authors and affiliations as list
+    # Sometimes authors and affiliations not read in as list
     if type(df.loc[2,'authors']) is not list:
         df['authors'] = df['authors'].apply(lambda x:x.split(";"))
         df['affiliations'] = df['affiliations'].apply(lambda x:x.split(";"))
         
-    ## "Explodes" Name and Affiliation columns - makes data by author-article, not article
+    # Explode Name and Affiliation cols. Key cols now author-article, not article.
     df = df.apply(pd.Series.explode)
     
     ## Extract emails
-    df['email'] = [extract_email_from_affiliations(affiliation) for affiliation in df['affiliations']]
+    df['email'] = [extract_email_from_affiliations(affiliation) 
+                       for affiliation in df['affiliations']]
     
-    ## Clean strings - take out stopwords, non-alphanumeric characters, and single  character words
+    # Clean strings
+    # Take out stopwords, non-alphanumeric chars, and single-character words
     if rm_stopwords:
-        df['title'] =  [clean_string(title,read_stoplist(title_stoplist_path))\
-          for title in df['title']]
-        df['affiliations'] =  [clean_string(affiliation,read_stoplist(affiliations_stoplist_path))\
-          for affiliation in df['affiliations']]
-        df['mesh_terms'] =  [clean_string(mesh_term,read_stoplist(mesh_stoplist_path), mesh = True)\
-          for mesh_term in df['mesh_terms']]
+        if stop_paths is None:
+            df['title'] =  [clean_string(title,read_stoplist(title_stop_path))
+                                for title in df['title']]
+            df['affiliations'] = [clean_string(affiliation,
+                                               read_stoplist(affil_stop_path))
+                                      for affiliation in df['affiliations']]
+            df['mesh_terms'] =  [clean_string(mesh_term,
+                                              read_stoplist(mesh_stop_path),
+                                              mesh = True)
+                                     for mesh_term in df['mesh_terms']]
+        elif len(stop_paths) == 3:
+            try:
+                df['title'] =  [clean_string(title,read_stoplist(stop_paths[0]))
+                                for title in df['title']]
+                df['affiliations'] = [clean_string(affiliation,
+                                               read_stoplist(stop_paths[1]))
+                                      for affiliation in df['affiliations']]
+                df['mesh_terms'] =  [clean_string(mesh_term,
+                                              read_stoplist(stop_paths[2]),
+                                              mesh = True)
+                                     for mesh_term in df['mesh_terms']]
+            except Exception:
+                raise Exception("Error while trying to read stoplists not"
+                                " defined in main() function.")
     else:
-        df['title'] =  [clean_string(title,rm_stopwords = False) for title in df['title']]
-        df['affiliations'] =  [clean_string(affiliation,rm_stopwords = False) for affiliation in df['affiliations']]
-        df['mesh_terms'] =  [clean_string(mesh_term,rm_stopwords = False) for mesh_term in df['mesh_terms']]
-    ## Save to a compressed csv in the results directory
+        df['title'] =  [clean_string(title,rm_stopwords = False) 
+                            for title in df['title']]
+        df['affiliations'] =  [clean_string(affiliation,rm_stopwords = False)
+                                   for affiliation in df['affiliations']]
+        df['mesh_terms'] =  [clean_string(mesh_term,rm_stopwords = False) 
+                                 for mesh_term in df['mesh_terms']]
+        
+    # Save to a compressed csv in the results directory
     df.to_csv(respath, compression = "gzip", sep=",")
     end = datetime.now() - begin
     print(filename + " - done - " + str(np.round(end.seconds / 60)) + "min")
     
-def start(text_data_dir, res_dir, nprocs=26):
+def start(text_data_dir, res_dir, nprocs=8):
     '''
     entry function
 
@@ -121,8 +183,8 @@ def start(text_data_dir, res_dir, nprocs=26):
     nprocs: number of cores in parallel
     '''
 
-    p = multiprocessing.Pool(processes=nprocs)
-    for dirpath, dirnames, filenames in os.walk(text_data_dir):
+    p = Pool(processes=nprocs)
+    for dirpath, _, filenames in os.walk(text_data_dir):
         for filename in filenames[:4]:
             if "gz" in filename and 'md5' not in filename and 'copy' not in filename:
                 filepath = os.path.join(dirpath, filename)
@@ -136,11 +198,29 @@ def start(text_data_dir, res_dir, nprocs=26):
     p.close()
     p.join()
 
-## The following appends all of the smaller zipped files together in a new "full" file.
-    #### TO DO: This runs on a single core. We can parallelize this if we make it a recursive function
-    # https://stackoverflow.com/questions/42246888/python-recursive-algorithm-multiprocess
-def combine(res_dir, combined_dir):
+
+def combine( res_dir, combined_dir, nprocs, keep_original_files = True):
+    """Append all of the smaller zipped files together in a new "full" file."""
+    
+    def append_files(filetuple):
+        """Worker function  appends two files together."""
+        if len(filetuple) != 2:
+            errmsg = ("append_files function expected 2 files to append,"
+                        "instead received" + len(filetuple) + ".")
+            raise Exception(errmsg)
+        file1 = pd.read_csv(filetuple[1], compression = "gzip", sep = ",")
+        
+        # Append to other file
+        file1.to_csv(filetuple[0], compression= "gzip", mode = "a", sep = ",")
+        os.remove(filetuple[1])
+        
+    def mute():
+        """Mute the output of Pool."""
+        sys.stdout = open(os.devnull, 'w') 
+        
     files = []
+    procpool = Pool(nprocs, initializer=mute)
+    # Populate a vector with filenames of data
     for dirpath, dirnames, filenames in os.walk(res_dir):
         for filename in filenames:
             print(filename)
@@ -148,62 +228,57 @@ def combine(res_dir, combined_dir):
                 filepath = os.path.join(dirpath, filename)
                 print(filepath)
                 files.append(filepath)
-    n = 0
-    respath = os.path.join(combined_dir, "full_pubmed.csv.gz")
-    for f in files:
-        if n ==0:
-            combined_df  = pd.read_csv(f,compression='gzip', sep = ",")
-            
-            combined_df.to_csv(respath, compression = "gzip",sep = ",")
-            del  combined_df
-        else:
-            combined_df = pd.read_csv(f,compression='gzip', sep = ",", lineterminator = '\n')
-            combined_df.to_csv(respath,compression='gzip', sep = ",", mode='a', header=False)  
-        n = n + 1   
-        print(n)
+
+    # Send off files two at a time to cores to be appended together.
+    # Put file names in a tuple and send them through with Pool.map
+    filetuples = list()
+    while len(files) > 1:
+        # Make an iterator of the files variable
+        iterfiles = iter(files)
+        for file in iterfiles:
+            # If our queue is full, send to processors
+            if len(filetuples) == nprocs:
+                procpool.map(append_files,filetuples, chunksize=1)
+                filetuples = list()
+
+            # If there is an odd file at the end of the list, empty queue
+            # Otherwise it'd be possible for repeats in the queue on next loop
+            if (file == files[-1]):
+                procpool.map(append_files,filetuples, chunksize=1)
+                filetuples = list()
+                break
+            else:
+                # Otherwise we add a new pair of files to be appended together
+                filetuples.append((file,next(iterfiles)))
+
+    # Change file name of combined dataset to full_pubmed.csv.gz
+    os.rename(files[0],(os.path.dirname(files[0]) + "full_pubmed.csv.gz"))
 
 
-def main(download_data = False, combine_data =  False):
-    '''
+def main(download_data = False, combine_data =  False, nprocs=8):
+    """
     text_data_dir: folder of raw data
     text_res_dir: folder of output
     verbose: int. Information is printed every N records
     nprocs: number of cores in parallel
-    '''
-    
+    """
+
     if download_data:
         download_pubmed_data()
-    ## Not sure if this is the best place,but we can curl in parallel (https://gist.github.com/CMCDragonkai/5914e02df62137e47f32)
-    ## Set the number of processors here
-    nprocs = 8
-    
-    ## Set the data directories
-        ## Text data dir is where the script finds the source xml file from the medline website
-    text_data_dir = "/Volumes/LaCie/PubmedData_Update_2019/ftp.ncbi.nlm.nih.gov/pubmed/baseline/"
-        ## Res dir is where the compressed, clean csv equivalents of each xml file is stored 
-    res_dir = "/Volumes/LaCie/PubmedData_Update_2019/Clean_Csvs/"
-        ## Combined dir is where 
-    combined_dir = "/Volumes/LaCie/PubmedData_Update_2019/Full_Pubmed/"
-    
-    ## Give the paths where stoplists are kept -- must be .txt files of words separated by commas and a space
-    
-        #### PATHS MUST BE ASSIGNED
-    title_stoplist_path = "/Users/RobertTeresi/Documents/pubmed_small_stopwords_list.txt"
-    affiliations_stoplist_path = "/Users/RobertTeresi/Documents/pubmed_affiliation_stopwords.txt"
-    mesh_stoplist_path =  "/Users/RobertTeresi/Documents/pubmed_mesh_stopwords.txt"
 
+    # If our results directory doesn't exist, we create it
     for temp_dir in [res_dir]:
         if not os.path.exists(temp_dir):
             os.makedirs(temp_dir)
     
-    ## Start reads, cleans, and saves the ~1050 xml files into csv.gz files
+    # start reads, cleans, and saves the ~1050 xml files into csv.gz files
     start(text_data_dir=text_data_dir, res_dir=res_dir, nprocs=nprocs)
     
+    # Combine appends all of the csvs into one, indexed by author-article
     if combine_data:
-        ## Combine appends all of the csvs into one, complete pubmed database, indexed by author-article
-        combine(res_dir=res_dir, combined_dir = combined_dir)
+        combine(res_dir=res_dir, combined_dir=combined_dir, nprocs=8)
 
 if __name__ == "__main__":
         main()
 
-## Make Coauthor Column look like 'D.ROY-0/S.LIN-0/J.LAGARENNE-0', maybe?
+## Make Coauthor Column look like 'D.ROY-0/S.LIN-0/J.LAGARENNE-0' (OK but why -0)
